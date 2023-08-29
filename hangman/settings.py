6c1b47960afd85b7b13e204775a817b4dd691959
JohnnysEmporium <44863546+JohnnysEmporium@users.noreply.github.com>
2023-08-29 14:47:41 +0200

    Update settings.py

--- hangman/settings.py
+++ hangman/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'ihd8ds+o!lcbt2kn=okzdk_hlkp6p!@t+2@bow1@9cd+yypuvr'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
